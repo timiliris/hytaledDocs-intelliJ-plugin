c4d4ce8168aa83b732ce46d52458754262828d37
Trevor Chumbley <chums122@gmail.com>
2026-02-12 11:25:38 -0800

    add contributor (self)

--- src/main/kotlin/com/hytaledocs/intellij/toolWindow/HytaleToolWindowFactory.kt
+++ src/main/kotlin/com/hytaledocs/intellij/toolWindow/HytaleToolWindowFactory.kt
@@ -695,6 +695,7 @@ class HytaleToolWindowPanel(
         // Contributors list
         val contributors = listOf(
             Triple("maartenpeels", "Mac & Linux support", "https://github.com/maartenpeels")
+            Triple("chums122", "Fix Maven support", "https://github.com/chums122")
         )
 
         contributors.forEach { (name, contribution, url) ->
